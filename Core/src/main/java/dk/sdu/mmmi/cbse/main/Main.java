7d93b231ade6e46c3071254870f602b724740c0b
MattisJensen <115666021+MattisJensen@users.noreply.github.com>
2024-05-04 15:00:28 +0200

    Rebase fix

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -7,6 +7,6 @@ import javafx.application.Application;
  */
 public class Main {
     public static void main(String[] args) {
-        Application.launch(GameLoopJavaFX.class);
+        Application.launch(GameLauncherJavaFX.class);
     }
 }
